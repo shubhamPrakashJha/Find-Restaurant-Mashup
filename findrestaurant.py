4574c2e7c9789d3e64237ce5092c1188e53f10c6
shubhamPrakashJha <prakash.shubhamjha@gmail.com>
2018-03-28 14:35:23 +0530

    add findrestaurant and main function

--- findrestaurant.py
+++ findrestaurant.py
@@ -10,3 +10,9 @@ sys.stderr = codecs.getwriter('utf8')(sys.stderr)
 # import foursquare client id & secret to send an API request
 foursquare_client_id = "WN0UC4JENWZZSTZROEOEJCZNLKYZO3QIEUBKA5GUDYB1IJ3U"
 foursquare_client_secret = "RT34CDWMWRZEL5DQH43P1F3A2HS0VP0OCTKRYZZFFAC2OSB0"
+
+def findRestaurant(mealType, location):
+    ''' return restaurant based on meal type and location '''
+
+if __name__ == '__main__':
+    findRestaurant("Pizza", "Tokyo, Japan")
