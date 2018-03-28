cefc2d966112f9a2011ff5b0a3cd1df6406eaed8
shubhamPrakashJha <prakash.shubhamjha@gmail.com>
2018-03-28 14:32:09 +0530

    save foursquare client id/secret

--- findrestaurant.py
+++ findrestaurant.py
@@ -7,3 +7,6 @@ import codecs
 sys.stdout = codecs.getwriter('utf8')(sys.stdout)
 sys.stderr = codecs.getwriter('utf8')(sys.stderr)
 
+# import foursquare client id & secret to send an API request
+foursquare_client_id = "WN0UC4JENWZZSTZROEOEJCZNLKYZO3QIEUBKA5GUDYB1IJ3U"
+foursquare_client_secret = "RT34CDWMWRZEL5DQH43P1F3A2HS0VP0OCTKRYZZFFAC2OSB0"
