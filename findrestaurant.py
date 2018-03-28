03355de97f098f3e313c19d3fb5f045a90a4383b
shubhamPrakashJha <prakash.shubhamjha@gmail.com>
2018-03-28 14:40:16 +0530

    get latitude and longitude

--- findrestaurant.py
+++ findrestaurant.py
@@ -4,6 +4,7 @@ import json
 
 import sys
 import codecs
+
 sys.stdout = codecs.getwriter('utf8')(sys.stdout)
 sys.stderr = codecs.getwriter('utf8')(sys.stderr)
 
@@ -11,8 +12,15 @@ sys.stderr = codecs.getwriter('utf8')(sys.stderr)
 foursquare_client_id = "WN0UC4JENWZZSTZROEOEJCZNLKYZO3QIEUBKA5GUDYB1IJ3U"
 foursquare_client_secret = "RT34CDWMWRZEL5DQH43P1F3A2HS0VP0OCTKRYZZFFAC2OSB0"
 
+
 def findRestaurant(mealType, location):
-    ''' return restaurant based on meal type and location '''
+    ''' return restaurant based on meal type and location ''' \
+    # Use getGeocodeLocation to get the latitude and longitude coordinates
+    #  of the location string.
+    coordinates = get_geocode(location)
+    lat = coordinates[0]
+    lng = coordinates[1]
+
 
 if __name__ == '__main__':
     findRestaurant("Pizza", "Tokyo, Japan")
