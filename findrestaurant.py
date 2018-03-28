4dc53888c9b21fb2e5be0638ca1488c46a284802
shubhamPrakashJha <prakash.shubhamjha@gmail.com>
2018-03-28 14:30:31 +0530

    convert non ascii character

--- findrestaurant.py
+++ findrestaurant.py
@@ -1,3 +1,9 @@
 from geocode import get_geocode
 import httplib2
 import json
+
+import sys
+import codecs
+sys.stdout = codecs.getwriter('utf8')(sys.stdout)
+sys.stderr = codecs.getwriter('utf8')(sys.stderr)
+
