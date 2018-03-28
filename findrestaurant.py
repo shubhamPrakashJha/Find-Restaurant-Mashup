881b8e68c3fedc3543f77e168336696a8a1cf1ac
shubhamPrakashJha <prakash.shubhamjha@gmail.com>
2018-03-28 17:39:38 +0530

    Beautify response Restaurant info

--- findrestaurant.py
+++ findrestaurant.py
@@ -33,42 +33,50 @@ def findRestaurant(mealType, location):
     h = httplib2.Http()
     response = h.request(url, "GET")
     body = json.loads(response[1])
-    restaurants = body['response']['venues']
+
 
     # Grab the first restaurant
-    restaurant = restaurants[0]
-    rname = restaurant['name']
-    rid = restaurant['id']
-    raddress = restaurant['location']['formattedAddress']
+    if body['response']['venues'] is not None:
+        restaurants = body['response']['venues']
+        restaurant = restaurants[0]
+        rname = restaurant['name']
+        rid = restaurant['id']
+        raddress = restaurant['location']['formattedAddress']
+        address = ""
+        for i in raddress:
+            address += i + " "
+        raddress = address
 
-    #  Get a  300x300 picture of the restaurant using the venue_id
-    # (you can change this by altering the 300x300 value in the URL or
-    # replacing it with 'orginal' to get the original picture
-    images_url = "https://api.foursquare.com/v2/venues/%s/photos?" \
-                 "client_id=%s&client_secret=%s&v=%s" % (
-                     rid, foursquare_client_id, foursquare_client_secret,
-                     version)
-    response = h.request(images_url, "GET")
-    body = json.loads(response[1])
-    if body['response']['photos']['count'] != 0:
-        # Grab the first image
-        image = body['response']['photos']["items"][0]
-        prefix = image['prefix']
-        suffix = image['suffix']
-        image_url = prefix + "300x300" + suffix
+
+        #  Get a  300x300 picture of the restaurant using the venue_id
+        # (you can change this by altering the 300x300 value in the URL or
+        # replacing it with 'orginal' to get the original picture
+        images_url = "https://api.foursquare.com/v2/venues/%s/photos?" \
+                     "client_id=%s&client_secret=%s&v=%s" % (
+                         rid, foursquare_client_id, foursquare_client_secret,
+                         version)
+        response = h.request(images_url, "GET")
+        body = json.loads(response[1])
+        if body['response']['photos']['count'] != 0:
+            # Grab the first image
+            image = body['response']['photos']["items"][0]
+            prefix = image['prefix']
+            suffix = image['suffix']
+            image_url = prefix + "300x300" + suffix
+        else:
+                # If no image is available, insert default a image url
+            image_url = "https://cdn.pixabay.com/photo/2018/03/27/09/46/wine-32" \
+                        "65462_960_720.jpg"
+
+        restaurant_info = {'name': rname, 'address': raddress,
+                           'image_url': image_url}
+        print "\nRestaurant Name: %s" % restaurant_info['name']
+        print "Restaurant Address: %s" % restaurant_info['address']
+        print "Image: %s" % restaurant_info['image_url']
+        return restaurant_info
     else:
-        # If no image is available, insert default a image url
-        image_url = "https://cdn.pixabay.com/photo/2018/03/27/09/46/wine-32" \
-                    "65462_960_720.jpg"
-
-    restaurant_info = {'name': rname, 'address': raddress,
-                       'image_url': image_url}
-    print "\nRestaurant Name: %s" % restaurant_info['name']
-    print "Restaurant Address: %s" % restaurant_info['address']
-    print "Image: %s" % restaurant_info['image_url']
-    return restaurant_info
-
-
+        print " No Restaurant Found in %s " % location
+        return "No Restaurant found"
 if __name__ == '__main__':
     findRestaurant("Pizza", "Tokyo, Japan")
     findRestaurant("Tacos", "Jakarta, Indonesia")
